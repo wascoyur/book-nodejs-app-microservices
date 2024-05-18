a2be2a9559ef9acd3a50aef5b43db30ba6c99179
yuriy.vasilev <mytem20007@gmail.com>
2024-05-18 07:05:28 +0500

    p.45 add orm

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UserModule, ConfigModule.forRoot() /*DatabaseModule*/],
+  imports: [UserModule, ConfigModule.forRoot(), DatabaseModule],
 })
 export class AppModule {}
