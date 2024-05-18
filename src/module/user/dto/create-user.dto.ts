8b5fdf664d48bbc75430c379bb06364d3f8406e0
yuriy.vasilev <mytem20007@gmail.com>
2024-05-18 16:47:43 +0500

    добавлены описания сваггер для юзера

--- src/module/user/dto/create-user.dto.ts
+++ src/module/user/dto/create-user.dto.ts
@@ -1,1 +1,57 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
+import { IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({
+    description: 'Идентификтор пользователя',
+    required: true,
+    type: String,
+  })
+  @Expose()
+  @IsString()
+  userId: string;
+
+  @ApiProperty({ description: 'User login', required: true, type: String })
+  @Expose()
+  @IsString()
+  login: string;
+
+  @ApiProperty({ description: 'User passwords', required: true, type: String })
+  @Expose()
+  @IsString()
+  password: string;
+
+  @ApiProperty({ description: 'User phone', required: true, type: String })
+  @Expose()
+  @IsString()
+  phone: string;
+
+  @ApiProperty({ description: 'User name', required: true, type: String })
+  @Expose()
+  @IsString()
+  firstName: string;
+
+  @ApiProperty({ description: 'Last User name', required: true, type: String })
+  @Expose()
+  @IsString()
+  lastName: string;
+
+  @ApiProperty({
+    description: 'Middle User name',
+    required: true,
+    type: String,
+  })
+  @Expose()
+  @IsString()
+  middleName: string;
+
+  @ApiProperty({
+    description: 'user e-mail',
+    required: true,
+    type: String,
+  })
+  @Expose()
+  @IsString()
+  email: string;
+}
